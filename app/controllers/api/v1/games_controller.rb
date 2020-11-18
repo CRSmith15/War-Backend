ba4110abc24c840d0c8b58d24fe4e1c2a6aff362
chance <chancesmith97@hotmail.com>
2020-11-18 10:56:43 -0700

    added index and create actions for games

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::GamesController < ApplicationController
+
+    def index 
+        @games = Game.all 
+        render json: @games 
+    end
+
+    def create
+        @user = User.find_by(name: params[:name])
+        Game.create(user_id: @user.id, pairs: params[:pairs])
+    end
 end
